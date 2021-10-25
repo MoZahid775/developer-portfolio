83ca00ca4844a72d0cd8af899b4cef65b268d362
Muhammad Zahid <muhammadali775@gmail.com>
2021-10-25 15:47:42 -0400

    changes urls

--- src/data/headerData.js
+++ src/data/headerData.js
@@ -1,10 +1,11 @@
 import resume from '../assets/pdf/resume.pdf'
 import resume2 from '../assets/pdf/resume2.pdf'
 
+
 export const headerData = {
     name: 'Mo Zahid',
     title: "Software Engineer",
     desciption:"I shall be telling this with a sigh... Somewhere ages and ages hence: Two roads diverged in a wood, and I— I took the one less traveled by, and that has made all the difference.",
-    image: 'https://scontent-lga3-1.xx.fbcdn.net/v/t1.6435-9/44098777_10156833071833914_8892707512667602944_n.jpg?_nc_cat=101&ccb=1-5&_nc_sid=174925&_nc_ohc=gEKrbe6jtWgAX_BtjfV&tn=0Eun2SAyT_qwtqhi&_nc_ht=scontent-lga3-1.xx&oh=85cefe89a74a996daab8d1bdfe012ef7&oe=616CE916',
+    image: 'https://scontent-lga3-1.xx.fbcdn.net/v/t1.6435-9/44098777_10156833071833914_8892707512667602944_n.jpg?_nc_cat=101&ccb=1-5&_nc_sid=174925&_nc_ohc=ctQIprP_PdEAX9-pC5p&tn=0Eun2SAyT_qwtqhi&_nc_ht=scontent-lga3-1.xx&oh=7979b4353ef1542e1777b1c59fef447c&oe=619C5F16',
     resumePdf: resume2
 }
--- src/data/projectsData.js
+++ src/data/projectsData.js
@@ -9,7 +9,7 @@ export const projectsData = [
         tags: ['React', 'Rails', 'Semantic Ui'],
         code: 'http://github.com/MoZahid775/clientology',
         demo: 'https://youtu.be/O8lf9FEiKko',
-        image: `https://scontent-lga3-1.xx.fbcdn.net/v/t1.6435-9/242182012_10159627646543914_679076295913363510_n.jpg?_nc_cat=102&ccb=1-5&_nc_sid=0debeb&_nc_ohc=qHVK4Egv8x8AX8yeq2Y&_nc_ht=scontent-lga3-1.xx&oh=fb3bac7000a1118f2363794d8d501141&oe=616D97ED`
+        image: `https://scontent-lga3-1.xx.fbcdn.net/v/t1.6435-9/242182012_10159627646543914_679076295913363510_n.jpg?_nc_cat=102&ccb=1-5&_nc_sid=0debeb&_nc_ohc=oN93rphavLcAX_11U8L&_nc_ht=scontent-lga3-1.xx&oh=01163b004e2b1fc92bdeb630a7beb3f3&oe=619D0DED`
     },
     {
         id: 2,
@@ -18,7 +18,7 @@ export const projectsData = [
         tags: ['React', 'CSS', 'Rails'],
         code: 'https://github.com/MoZahid775/open-chessame',
         demo: 'https://youtu.be/o2qxblmATNI',
-        image: 'https://scontent-lga3-1.xx.fbcdn.net/v/t1.6435-9/242175195_10159627646533914_8906297445132198235_n.jpg?_nc_cat=111&ccb=1-5&_nc_sid=0debeb&_nc_ohc=T9oxkYP7zfYAX8nBnOH&_nc_ht=scontent-lga3-1.xx&oh=d3fe615920cab95ff7bc1cbb9dc0e523&oe=616DFDE8'
+        image: 'https://scontent-lga3-1.xx.fbcdn.net/v/t1.6435-9/242175195_10159627646533914_8906297445132198235_n.jpg?_nc_cat=111&ccb=1-5&_nc_sid=0debeb&_nc_ohc=YBr_MFdBac8AX9WYfFC&_nc_ht=scontent-lga3-1.xx&oh=36f7622cfe0f377cca242c43731f083d&oe=619D73E8'
     },
     {
         id: 3,
@@ -27,7 +27,7 @@ export const projectsData = [
         tags: ['React', 'Ruby', 'Sinatra Framework'],
         code: 'https://github.com/MoZahid775/arnold',
         demo: 'https://youtu.be/rcs8YSIO4o4',
-        image: 'https://scontent-lga3-1.xx.fbcdn.net/v/t1.6435-9/242207255_10159627634273914_911585231145010019_n.jpg?_nc_cat=101&ccb=1-5&_nc_sid=0debeb&_nc_ohc=FKpPyL9kgI0AX9V0s1q&_nc_ht=scontent-lga3-1.xx&oh=0a0fba89c40fd7c7fe7d73c2c2acb4ae&oe=616F910B'
+        image: 'https://scontent-lga3-1.xx.fbcdn.net/v/t1.6435-9/242207255_10159627634273914_911585231145010019_n.jpg?_nc_cat=101&ccb=1-5&_nc_sid=0debeb&_nc_ohc=XJPS26_2ASQAX-M3X_q&_nc_ht=scontent-lga3-1.xx&oh=4901f8f818c457652c51c3757fb5c774&oe=619B128B'
     }
 ]
 
